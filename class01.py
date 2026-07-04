0294980dea3bba0804b46058860e4be698740b9d
Krypton315 <leooo89990@gmail.com>
2026-07-04 19:01:31 +0800

    Add files via upload

---
+++ class01.py
@@ -0,0 +1,20 @@
+class Student:
+	def __init__(self, name, student_id):
+		self.name = name
+		self.student_id = student_id
+		self.scores = []
+	
+	def add_score(score):
+		self.scores.append(scores)
+
+	def average_score():
+		if not self.scores:
+			return 0
+		average = sum(self.scores) / len(self.scores)
+		return average
+
+	def get_grade():
+		pass
+
+	def display():
+		pass
